0eac4f6ab6dd0b06dec3d8c12c40755b5781549f
Giovanni Stevanato <stevanay@gmail.com>
2021-12-06 18:45:51 +0100

    added internet permission to manifest and testing http requests to comunicate with database

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -8,6 +9,12 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.myapplication.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -31,6 +38,39 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        loadUserProfile();
+    }
+
+    private void loadUserProfile(){
+
+        final TextView usernameFill = (TextView) findViewById(R.id.UsernameFill);
+        final TextView emailFill = (TextView) findViewById(R.id.EmailFill);
+        final TextView nameFill = (TextView) findViewById(R.id.NameFill);
+        final TextView surnameFill = (TextView) findViewById(R.id.SurnameFill);
+        final TextView birthDateFill = (TextView) findViewById(R.id.NameFill);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://locahost:3300/users/";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        usernameFill.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                usernameFill.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
     }
 
 }
