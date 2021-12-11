ffea1b8b7b2d4ea289e20cf323533c38f6040d3a
Giovanni Stevanato <stevanay@gmail.com>
2021-12-11 13:00:10 +0100

    User input images in create annoncement implemented

--- app/src/main/java/com/example/myapplication/ui/home/CreateAnnouncementFragment.java
+++ app/src/main/java/com/example/myapplication/ui/home/CreateAnnouncementFragment.java
@@ -1,17 +1,18 @@
 package com.example.myapplication.ui.home;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.myapplication.R;
-import com.example.myapplication.databinding.FragmentAnnouncementBinding;
 import com.example.myapplication.databinding.FragmentCreateAnnouncementBinding;
 
 public class CreateAnnouncementFragment extends Fragment {
@@ -40,6 +41,30 @@ public class CreateAnnouncementFragment extends Fragment {
     }
 
     private void loadCreateAnnouncement(View view){
-        //setup usefull stuff
+        view.findViewById(R.id.addPhotoButton).setOnClickListener(v -> {
+            Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            pickPhoto.setType("image/*");
+            startActivityForResult(pickPhoto , 1);
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == -1 && requestCode == 1){
+
+            //retrive Image
+            ImageView img = new ImageView(getContext());
+            LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            p.setMargins(20,20,20,20);
+            img.setLayoutParams(p);
+            img.setImageURI(data.getData());
+
+
+            //add image to photoList
+            LinearLayout photoList = getView().findViewById(R.id.photoList);
+            photoList.addView(img);
+
+        }
     }
 }
