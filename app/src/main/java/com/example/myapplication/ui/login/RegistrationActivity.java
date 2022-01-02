67e0d1e26305df81b2ef2225caa01dda5781b793
Giovanni Stevanato <stevanay@gmail.com>
2022-01-02 18:17:15 +0100

    UI finished for login and registration

--- app/src/main/java/com/example/myapplication/ui/login/RegistrationActivity.java
+++ app/src/main/java/com/example/myapplication/ui/login/RegistrationActivity.java
@@ -2,15 +2,50 @@ package com.example.myapplication.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
+import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class RegistrationActivity extends AppCompatActivity {
+
+    final private Calendar myCalendar= Calendar.getInstance();
+    private EditText birthDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        this.birthDate = findViewById(R.id.enterSignUpBirthDate);
+        DatePickerDialog.OnDateSetListener date =new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,month);
+                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                updateLabel();
+            }
+        };
+        this.birthDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(RegistrationActivity.this,date,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void updateLabel(){
+        String myFormat="MM/dd/yy";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
+        this.birthDate.setText(dateFormat.format(myCalendar.getTime()));
     }
 }
