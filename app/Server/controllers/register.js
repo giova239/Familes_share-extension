d3425002afd6b244c6e7c4216c713b1800f0648d
AlexCondurache <43399860+AlexCondurache@users.noreply.github.com>
2022-01-05 18:01:01 +0100

    add createGroup & allGroups to create and display group

--- app/Server/controllers/register.js
+++ app/Server/controllers/register.js
@@ -28,11 +28,7 @@ exports.register = async (req, res) => {
                 if(credentials.validate(surname)){
                     if(emailvalidator.validate(email)){
                         if(pwdValidator.validate(password)){
-                            if (arr.length != 0) {
-                                return res.status(400).json({
-                                    error: "Email already there, No need to register again.",
-                                });
-                            }
+                            if (arr.length != 0) {return res.status(400).json({ error: "Email already there, No need to register again.",});}
                             else {
                                 bcrypt.hash(password, 10, (err, hash) => {
                                     if (err)
@@ -49,7 +45,6 @@ exports.register = async (req, res) => {
                                         phone
                                     };
                                     //Inserting data into the database
-
                                     client.query(`INSERT INTO "Users" (name, surname, email, password, birth_date, phone) VALUES ($1,$2,$3,$4,$5,$6);`, [user.name, user.surname, user.email, user.password, user.birth_date, user.phone], (err) => {
                                         if (err) {
                                             console.error(err);
@@ -57,41 +52,19 @@ exports.register = async (req, res) => {
                                                 error: "Database error"
                                             })
                                         }
-                                        else {
-                                            res.status(200).send({
+                                        else {res.status(200).send({
                                                 message: 'User added to database, not verified',
                                                 id_user: id_user,
                                             });
                                         }
                                     })
-
                                 });
                             }
-                        }else{
-                            res.status(400).json({
-                                error: 'Invalid Password, the password must be 8 characters long and have 1 uppercase, 1 lowercase character and 1 number!'
-                            });
-                        }
-                    }else{
-                        res.status(400).json({
-                            error: 'Invalid Email. Please insert a valid email format!'
-                        });
-                    }
-                }else{
-                    res.status(400).json({
-                        error: 'Surname cannot be empty. Please enter a valid surname!'
-                    });
-                }
-           }else{
-               res.status(400).json({
-                   error: 'Date cannot be empty. Please enter a valid date!'
-               });
-           }
-        }else{
-            res.status(400).json({
-                error: 'Name cannot be empty. Please enter a valid name!'
-            });
-        }
+                        }else{res.status(400).json({error: 'Invalid Password, the password must be 8 characters long and have 1 uppercase, 1 lowercase character and 1 number!'});}
+                    }else{res.status(400).json({error: 'Invalid Email. Please insert a valid email format!'});}
+                }else{res.status(400).json({error: 'Surname cannot be empty. Please enter a valid surname!'});}
+           }else{res.status(400).json({error: 'Date cannot be empty. Please enter a valid date!'});}
+        }else{res.status(400).json({error: 'Name cannot be empty. Please enter a valid name!'});}
     }
     catch (err) {
         console.log(err);
